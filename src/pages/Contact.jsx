0ff123d72e6e5bb93233d5ba15f788334acad3a4
dhia_raboudi <mohameddhia.raboudi@etudiant-enit.utm.tn>
2024-12-07 19:27:49 +0100

    Map done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import { Contact1 } from "../components/Contact1";
 import { ContactForm } from "../components/ContactForm";
+import { Map } from "../components/Map";
 export const Contact = () => {
   return (
     <div>
       <Contact1 />
       <ContactForm />
+      <Map />
     </div>
   )
 }
