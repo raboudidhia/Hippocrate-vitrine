943c666b13920803bbabc0c147ceece05a56bef4
fedibbm <benbrahimfedi4@gmail.com>
2024-12-08 12:33:37 +0100

    increased vertical gap in contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -40,7 +40,7 @@ export const ContactForm = () => {
         </div>
 
         <div className="flex-1 p-6 md:p-8">
-          <form className="flex flex-col">
+          <form className="flex flex-col gap-4">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
               <div>
                 <label
